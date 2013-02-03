3bb227421170c9c02ea4c0c2c2ba4db8bc25f82e
iwasinnam <iwasinnam@gmail.com>
2013-02-03 19:55:26 +0900

    Added GUI (on old version)

--- bulkEmailer.py
+++ bulkEmailer.py
@@ -3,12 +3,18 @@
 import xml.etree.ElementTree as ET # parse xml file with mail content
 import os
 import fnmatch # help locate xml files
+import pickle # for persitence
+import time
+
+from urlparse import urlparse
+from ftplib import FTP # download bulk
 
 # Import smtplib for the actual sending function
 import smtplib
 
 # Import the email modules we'll need
 from email.mime.text import MIMEText
+import email.header # used for debugging
 
 class Bulk:
     """An abstraction for the xml files with the mail content.
@@ -17,18 +23,22 @@ class Bulk:
     def __init__(self,location):
         """Prepare a list of valid xml files in the folder
         """
+        self.loc = location
         self.xmlNames = []
-        for file in os.listdir(location):
+        for file in os.listdir(self.loc):
             if fnmatch.fnmatch(file, u'*.xml') \
-            and ET.parse(file).getroot().tag == 'email_msg':
+                and ET.parse(os.path.join(self.loc,file)) \
+                .getroot().tag == 'email_msg':
                 self.xmlNames +=[file]
-            
+        if len(self.xmlNames) <= 0:
+            return
+        
         # todo: improve sort (this one is buggy)
         self.xmlNames.sort()
         
         # immediately load first file
         self.oldXmlName = self.fName = self.xmlNames[0]
-        self.root = ET.parse(self.fName)
+        self.root = ET.parse(os.path.join(self.loc, self.fName))
         
     def recepients(self):
         """Iterable list of recepients.
@@ -36,11 +46,12 @@ class Bulk:
         for xmlName in self.xmlNames:
             if xmlName != self.fName:
                 self.fName = xmlName
-                self.root = ET.parse(self.fName)
+                self.root = ET.parse(os.path.join(self.loc, self.fName))
             for email_to in self.root.findall(u'email_to'):
                 yield email_to
                 self.oldXmlName = self.fName
-            #todo: erase file (maybe not here though)
+            # assuming sending succeeded, erase XML file (TEMPORARILY DISABLED)
+            # os.remove(os.path.join(self.loc, self.fName))
         # todo: consider persistence
     
     def content(self,mailtype='text'):
@@ -79,27 +90,34 @@ class Sender:
         self.bulk = bulk
                 
     def _start(self):
-        self.s = smtplib.SMTP(*bulk.smtp_server())
-        self.s.login(*bulk.smtp_credentials())
-        # Create a text/plain message
-        self.msg = MIMEText(bulk.content())
-        self.msg['Subject'] = bulk.subject()
-        self.msg['From'] = u'"' + bulk.sender()['name'] + u'" ' \
-            u'<' + bulk.sender()['addr'] + u'>'
+        try:
+            self.s = smtplib.SMTP(*self.bulk.smtp_server())
+            self.s.login(*self.bulk.smtp_credentials())
+            # Create a text/plain message
+            self.msg = MIMEText(self.bulk.content())
+            self.msg['Subject'] = self.bulk.subject()
+            self.msg['From'] = (u'"' + self.bulk.sender()['name'] + u'" ' +
+                u'<' + self.bulk.sender()['addr'] + u'>')
+            print (u'Preparing to send mail "' 
+                + email.header.decode_header(self.msg['Subject'])[0][0] +'"...')
+        except AttributeError:
+            return
     
     def _finish(self):
-        if self.s:
+        try:
             self.s.quit()
+        except AttributeError as e:
+            return
     
     def restart(self):
         self._finish()
         self._start()
 
 
-    def run(self):
+    def send_iter(self):
         try:
             self._start()
-            for email_to in bulk.recepients():
+            for email_to in self.bulk.recepients():
                 if self.bulk.changed():
                     self.restart()
                 
@@ -107,15 +125,82 @@ class Sender:
                 self.msg['To'] = '"' \
                     + email_to.find(u'name_encoded').text \
                     + u'" <' + email_to.attrib[u'addr'] + u'>'
+                # send the mail
+                # todo: handle errors (esp. by retrying)
+                # send mail
                 self.s.sendmail(self.msg['From'], self.msg['To'], \
                     self.msg.as_string())
-                print self.msg.as_string()
-        
+                yield
+#        except Exception as e:
+#            print e
         finally:
             self._finish()
+    
+    def run(self):
+        for x in self.send_iter():
+            print "sent to ", self.msg['To']
+
+class BulkDownloader:
+    def __init__(self, url, user='anonymous', pw='', \
+        ext='.xml',target='.', crmUserId = 26):
+        self.crmUserId = crmUserId
+        self.o = urlparse(url)
+        if self.o.scheme != 'ftp':
+            raise ValueError('only ftp supported')
+        if self.o.path[-1:] != '/':
+            raise ValueError('only directories supported')
+        self.user = user
+        self.pw = pw
+        self.ext = ext
+        self.downloaded = [] # list of downloaded files
+        
+        # create target path if it doesn't exist
+        if not os.access(target,os.F_OK):
+            os.makedirs(target)
+        self.target = target
+
+    def download(self):
+        for x in self.download_iter():
+            print self.msg
+
+    # break downloading into smaller steps, to allow a more flexible UI
+    def download_iter(self):
+        ftp = FTP()
+        try:
+            self.msg = 'conecting to ' + self.o.netloc
+            ftp.connect(self.o.netloc)
+            yield
+            self.msg =  'logging in as ' + self.user
+            ftp.login(self.user,self.pw)
+            yield
+            self.msg = 'accessing ' + self.o.path
+            ftp.cwd(self.o.path)
+            yield
+            for fname in ftp.nlst():
+                if fname[-len(self.ext):] != self.ext:
+                    continue #only download .xml files
+                if int(fname[0:fname.find('_')]) != self.crmUserId:
+                    continue # only download files created by this user
+                
+                with open(os.path.join(self.target,fname),'w') as f:
+                    ftp.retrbinary('RETR ' + fname, lambda c : f.write(c))
+                self.downloaded += [fname]
+                self.msg = 'downloaded ' + fname
+                yield
+        finally:
+            print "ftp disconnected"
+            ftp.quit()
+     
+
 
 if __name__ == "__main__":
-    bulk = Bulk(u'.')
+    # download xml files of current user
+    d = BulkDownloader('ftp://ftp.servage.net/', \
+        user='baruchhashem',pw='tribal12',target=u'inbox', crmUserId=26)
+    d.download()
     
+    # send all files in the inbox
+    bulk = Bulk(u'inbox')
     s = Sender(bulk)
     s.run()
+
---
+++ bulkEmailerGui.py
@@ -0,0 +1,128 @@
+﻿#!/usr/bin/env python
+
+# bulkEmailerGui.py
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+import gobject
+import threading
+import email.header # used for debugging
+
+
+from bulkEmailer import Bulk
+from bulkEmailer import BulkDownloader
+from bulkEmailer import Sender
+
+ 
+class FuncThread(threading.Thread):
+    def __init__(self, target, *args):
+        self._target = target
+        self._args = args
+        threading.Thread.__init__(self)
+ 
+    def run(self):
+        self._target(*self._args)
+ 
+
+class MainWindow:
+    
+    def delete_event(self, widget, event=None, data=None):
+        self.abort = True
+        gtk.main_quit()
+        return False
+    
+    def run(self):
+        self.abort = False
+        self.run_thread = FuncThread(MainWindow.background_run, self)
+        self.run_thread.start()
+        
+    def stop(self):
+        self.abort = True
+        self.stop_button.set_sensitive(False)
+        
+    def inform(self,msg):
+        print msg
+        gobject.idle_add(gtk.Label.set_text,self.stat_label,msg)
+            
+    def background_run(self):
+        try:
+            gobject.idle_add(gtk.Button.hide, self.go_button)
+            gobject.idle_add(gtk.Button.set_sensitive, self.stop_button, True)
+            gobject.idle_add(gtk.Button.show, self.stop_button)
+        
+            # download xml files of current user
+            self.d = BulkDownloader('ftp://ftp.servage.net/',
+                user='baruchhashem',pw='tribal12',target=u'inbox', crmUserId=26)
+            gobject.idle_add(gtk.ProgressBar.pulse, self.progbar)
+
+            for x in self.d.download_iter():
+                if self.abort:
+                    return
+                self.inform(self.d.msg)
+                gobject.idle_add(gtk.ProgressBar.pulse, self.progbar)
+
+            self.inform(U'downloaded '+ str(self.d.downloaded))
+
+            # send all files in the inbox
+            bulk = Bulk(u'inbox')
+
+            self.s = Sender(bulk)
+            for x in self.s.send_iter():
+                if self.abort:
+                    return
+                gobject.idle_add(gtk.ProgressBar.pulse, self.progbar)
+                to_name, _, to_addr = self.s.msg['To'].partition(' ')
+                self.inform(U'sent "' 
+                    + email.header.decode_header(self.s.msg['Subject'])[0][0]
+                    + U'" to ' 
+                    + email.header.decode_header(to_name.strip('"'))[0][0] 
+                    + U' ' + to_addr)
+    
+        finally:
+            gobject.idle_add(gtk.Button.hide, self.stop_button)
+            gobject.idle_add(gtk.Button.show, self.go_button)
+            gobject.idle_add(gtk.Button.set_sensitive, self.go_button, True)
+            gobject.idle_add(gtk.ProgressBar.set_fraction, self.progbar, 0.0)
+            gobject.idle_add(gtk.Label.set_text,self.stat_label,'')
+
+            
+    def __init__(self):
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.set_title(U"Bulk Emailer")
+        self.window.connect("destroy", lambda wid: gtk.main_quit())
+        self.window.connect("delete_event", self.delete_event)
+        self.window.set_size_request(400,100)
+        
+        vbox = gtk.VBox(False,0)
+        vbox.set_border_width(10)
+        self.window.add(vbox)
+        vbox.show()
+        
+        self.go_button = gtk.Button(label=U"Go")
+        self.go_button.connect("clicked", lambda _: self.run())
+        vbox.pack_start(self.go_button, False, False, 0)
+        self.go_button.show()
+        
+        self.stop_button = gtk.Button(label=U"Stop")
+        self.stop_button.connect("clicked", lambda _: self.stop())
+        vbox.pack_start(self.stop_button, False, False, 0)
+        self.stop_button.hide()
+        
+        self.progbar = gtk.ProgressBar()
+        vbox.pack_start(self.progbar, False, False, 0)
+        self.progbar.show()
+        
+        self.stat_label = gtk.Label()
+        self.stat_label.set_line_wrap(False)
+        self.stat_label.set_justify(gtk.JUSTIFY_LEFT)
+        vbox.pack_start(self.stat_label, True, True, 0)
+        self.stat_label.show()
+        
+        self.window.show()
+
+        
+if __name__ == "__main__":
+    gobject.threads_init()
+    main = MainWindow()
+    gtk.main()
